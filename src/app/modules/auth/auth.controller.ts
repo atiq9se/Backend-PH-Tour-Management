c4ae000dabbd712654f718385fa2795817c8f1bb
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-08-19 03:01:04 +0600

    Create utility functions for getting accessToken and refreshToken and setting browser cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,19 +5,21 @@ import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import { AuthServices } from './auth.service';
 import AppError from '../../errorHelpers/AppError';
+import { setAuthCookie } from '../../utils/setCookie';
 
 const credentialsLogin = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    res.cookie("accessToken", loginInfo.accessToken,{
-        httpOnly: true,
-        secure: false
-    })
+    // res.cookie("accessToken", loginInfo.accessToken,{
+    //     httpOnly: true,
+    //     secure: false
+    // })
+    // res.cookie("refreshToken", loginInfo.refreshToken,{
+    //     httpOnly: true,
+    //     secure: false
+    // })
 
-    res.cookie("refreshToken", loginInfo.refreshToken,{
-        httpOnly: true,
-        secure: false
-    })
+    setAuthCookie(res, loginInfo)
 
     sendResponse(res, {
         success: true,
@@ -34,6 +36,8 @@ const getNewAccessToken = catchAsync(async(req: Request, res: Response, next: Ne
     }
 
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+
+    setAuthCookie(res, tokenInfo)
 
     sendResponse(res, {
         success: true,
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,7 +6,7 @@ import bcryptjs from "bcryptjs";
 import { JwtPayload } from "jsonwebtoken";
 import { generateToken, verifyToken } from '../../utils/jwt';
 import { envVars } from '../../config/.env';
-import { creteUserTokens } from '../../utils/userTokens';
+import { createNewAccessTokenWithRefreshToken, creteUserTokens } from '../../utils/userTokens';
 
 
 const credentialsLogin = async(payload: Partial<IUser>) => {
@@ -48,30 +48,9 @@ const credentialsLogin = async(payload: Partial<IUser>) => {
 }
 
 const getNewAccessToken = async(refreshToken: string) => {
-   const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload
-
-   const isUserExist = await User.findOne({email: verifiedRefreshToken.email})
-   
-    if(!isUserExist){
-        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
-    }
-    if(isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE){
-        throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
-    }
-    if(isUserExist.isDeleted){
-        throw new AppError(httpStatus.BAD_REQUEST, "User is Deleted")
-    }
-
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
-
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
-    
+   const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken)
     return {
-        accessToken
+        accessToken: newAccessToken
     }
 
 }

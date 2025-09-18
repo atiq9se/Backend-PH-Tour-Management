456db3f7bc57b5595ab95534799e52a863620fb3
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-09-19 00:10:22 +0600

    booking session part adding

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+import { Tour } from './../tour/tour.model';
 import { Payment } from './../payment/payment.model';
 import { Booking } from './booking.model';
 import  httpStatus  from 'http-status-codes';
@@ -11,17 +12,21 @@ const getTransactionId = () =>{
 }
 
 const createBooking = async (payload : Partial<IBooking>, userId: string)=>{
-    const user = await User.findById(userId);
-
-
-    
     const transactionId = getTransactionId()
 
+    const session = await Booking.startSession();
+
+    session.startTransaction()
+
+    try{
+    const user = await User.findById(userId, session);
+    
     if(!user?.phone || !user.address){
        throw new AppError(httpStatus.BAD_REQUEST, "Please update your porfile to book a tour")
     }
 
     const tour = await Tour.findById(payload.tour).select("costFrom")
+
     if(!tour?.costFrom){
         throw new AppError(httpStatus.BAD_REQUEST, "No tour cost found")
     }
@@ -33,7 +38,7 @@ const createBooking = async (payload : Partial<IBooking>, userId: string)=>{
         user: userId,
         status: BOOKING_STATUS.PENDING,
         ...payload
-    })
+    }, {session})
 
     const payment = await Payment.create({
         booking: booking._id,
@@ -49,7 +54,17 @@ const createBooking = async (payload : Partial<IBooking>, userId: string)=>{
         {new:true, runValidators: true}
     ).populate("user").populate("tour").populate("payment");
 
+    await session.commitTransaction();
+    session.endSession()
+
     return updateBooking
+    }
+    catch(error: any){
+        await session.abortTransaction();
+        session.endSession()
+    }
+
+    
 }
 
 const getUserBookings = async () =>{
